b4d725bff6a04bc4c5706969ba5aae58047eb470
phamtanphat <phatdroid94@gmail.com>
2022-01-03 18:45:14 +0700

    stop service

--- app/src/main/java/com/example/forcegroundservice18102021/MyService.java
+++ app/src/main/java/com/example/forcegroundservice18102021/MyService.java
@@ -7,6 +7,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -18,6 +19,7 @@ public class MyService extends Service {
     String CHANNEL_ID = "mychannel";
     NotificationManager mNotificationManager;
     Notification mNotification;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -27,30 +29,30 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("BBB","OnCreate");
+        Log.d("BBB", "OnCreate");
         mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        mNotification = createNotification(this,"Khởi tạo lần đầu");
-        mNotificationManager.notify(1,mNotification);
-        startForeground(1,mNotification);
+        mNotification = createNotification(this, "Khởi tạo lần đầu");
+        mNotificationManager.notify(1, mNotification);
+        startForeground(1, mNotification);
 
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("BBB","onStartCommand");
-        mNotification = createNotification(this,"Xử lý");
-        mNotificationManager.notify(1,mNotification);
-        return START_REDELIVER_INTENT;
+//        Log.d("BBB","onStartCommand");
+        mNotification = createNotification(this, "Xử lý");
+        mNotificationManager.notify(1, mNotification);
+        return START_NOT_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("BBB","onDestroy");
+        Log.d("BBB", "onDestroy");
     }
 
-    private Notification createNotification(Context context , String message){
-        NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(context,CHANNEL_ID);
+    private Notification createNotification(Context context, String message) {
+        NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(context, CHANNEL_ID);
         notificationCompat.setContentTitle("Thông báo");
         notificationCompat.setContentText(message);
         notificationCompat.setShowWhen(true);
